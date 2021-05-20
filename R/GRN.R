bcd60c16664ac02bffbb74044b049ce1e0435bc0
Gavin-Lijy <jingyu.gavin.li@outlook.com>
2021-05-20 16:31:44 +0800

    Update GRN.R

--- R/GRN.R
+++ R/GRN.R
@@ -1,4 +1,17 @@
-runGRNboost=function(obj,path.to.pyscenic,gmat_file,tf_file,adj_file='adj.csv',out_file='adj_add_cor.csv',min_importance=2,only_pos=TRUE){
+
+gmat=FilterGmat<-function(obj,hvg=2000){
+  gmat=obj@gmat[!is.na(rownames(gmat)),]
+  library(Seurat)
+  seu=CreateSeuratObject(counts = gmat)
+  seu=NormalizeData(seu)
+  seu=FindVariableFeatures(seu,nfeatures = hvg)
+  VariableFeaturePlot(seu)
+  gmat=seu@assays$RNA@data
+  return(gmat)
+}
+
+runGRNboost=function(gmat,path.to.pyscenic,gmat_file='gmat.csv',tf_file,adj_file='adj.csv',out_file='adj_add_cor.csv',min_importance=2,only_pos=TRUE){
+  write.csv(gmat,file = gmat_file)
   system(paste(path.to.pyscenic,'grn',gmat_file,tf_file,'-o',adj_file))
   system(paste(path.to.pyscenic,'add_cor',adj_file,gmat_file,'-o',out_file))
   regulon<-read.csv(out_file)
@@ -8,3 +21,4 @@ runGRNboost=function(obj,path.to.pyscenic,gmat_file,tf_file,adj_file='adj.csv',o
   regulon<-regulon[regulon$importance>min_importance,]
   return(regulon)
 }
+
